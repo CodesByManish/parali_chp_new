129d30b5d9035ea65725c51ccf7d58d180b40218
manish laxane <manishlaxane2017@gmail.com>
2025-07-29 16:20:27 +0530

    first commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const WebSocket = require('ws');
 
 // --- Modbus Configuration ---
 const client = new ModbusRTU();
-const ipAddress = "192.168.200.34"; // Replace with your device's IP
+const ipAddress = "192.168.200.35"; // Replace with your device's IP
 //const ipAddress = "192.168.100.1"; // Replace with your device's IP
 const modbusPort = 502; // Modbus TCP port
 const unitId = 1;
